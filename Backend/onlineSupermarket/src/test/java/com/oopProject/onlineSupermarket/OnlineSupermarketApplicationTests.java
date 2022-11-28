351238bbd0d16f75aa6cce228072339c0399a8a1
Ayush Raj <92251019+rajAyushh@users.noreply.github.com>
2022-11-28 16:47:49 +0530

    Add files via upload

---
+++ Backend/onlineSupermarket/src/test/java/com/oopProject/onlineSupermarket/OnlineSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopProject.onlineSupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
