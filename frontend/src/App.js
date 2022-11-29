94e4ec146e8872ec845d7ba985c87891fdd6e1ac
shreyanssoni <soni21.shreyans@gmail.com>
2022-11-29 23:54:13 +0530

    Frontend Updates-29Nov

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import axios, {isCancel, AxiosError} from 'axios';
-import Login from './pages/login';
+import Login from './pages/Login';
 import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   let [isLogin, setIsLogin] = useState(false)
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+// import styles from './Login.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 
 export default function Login() {
-  return (<>
-    <div>Login</div>
+  const [user, setUser] = useState([]);
+  return (
+    <>
+    <div className='d-flex justify-content-center align-items-center' style={{height:'100vh'}}>
+    <Card className='mx-2 w-25 my-auto mx-auto d-flex'>
+      <Card.Title className='mx-auto mt-4'>LOGIN</Card.Title>
+      <Card.Body className='mx-2'>
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+      </Card.Body>
+    </Card>
+    </div>
     </>
   )
 }
