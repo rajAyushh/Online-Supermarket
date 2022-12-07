2f2160e4c5c240ded4d1f517c9d7b1979d53e87e
Ayush Raj <ayushcode1@gmail.com>
2022-12-07 06:23:20 +0530

    Added sql integration

--- onlineSupermarket/src/main/java/com/oopProject/onlineSupermarket/OnlineSupermarketApplication.java
+++ onlineSupermarket/src/main/java/com/oopProject/onlineSupermarket/OnlineSupermarketApplication.java
@@ -2,12 +2,24 @@ package com.oopProject.onlineSupermarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class OnlineSupermarketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineSupermarketApplication.class, args);
+	}
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create()
+				.url("jdbc:mysql://localhost:3306/online_supermarket_db?useSSL=FALSE")
+				.username("root")
+				.password("Passy@123")
+				.build();
 	}
 
 }
